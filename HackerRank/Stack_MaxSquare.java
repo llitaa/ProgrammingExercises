1e0b553e73afe26c0b54d79417f026a1b9abbb69
Anton Lytvynenko <anton.lytvynenko@brainlab.net>
2018-02-23 16:29:36 +0100

    Resolved max square problem

--- HackerRank/Stack_MaxSquare.java
+++ HackerRank/Stack_MaxSquare.java
@@ -12,14 +12,12 @@ public class Solution {
         Node (int ht, int ind) {
             height = ht;
             index = ind;
-            // System.out.printf("Init node %d %d", height, index);
         }
     }
     
     //! Computes max rect square with node passed
     static long computeHeightSquare(int height, int startInd, int endInd) {
         long res = height * (endInd - startInd + 1);
-        // System.out.printf("Update max: ht %d, sInd %d, eInd %d %n", height, startInd, endInd);
         return res;
     }
     
@@ -28,45 +26,38 @@ public class Solution {
     }
         
     static long largestRectangle(int[] h) {
-        //int[] h = {3,2,5,6,1,4,4};
-        
         // Complete this function
+        int stackSize = 0;
         Stack<Node> stack = new Stack<Node>();
         long res = 0;
         for(int i=1; i<=h.length; ++i) {
             // System.out.println(i);
             int height = h[i-1];
-            if(stack.isEmpty()) {
-                stack.push(new Node(height, i));
-                continue;
+            int prevHeight = stackSize == 0 ? -1 : stack.peek().height;
+            int itemInd = i;
+            if(height < prevHeight) { 
+                int lastInd = stackSize;
+                while(stackSize != 0) { // found bigger item
+                    Node currTop = stack.peek();
+                    if(currTop.height > height) {
+                        res =  updateMax(res, computeHeightSquare(currTop.height, currTop.index, i-1));
+                        itemInd = currTop.index;
+                        stack.pop();
+                        --stackSize;
+                    }
+                    else {
+                        break;
+                    }  
+                }
             }
-            Node top = stack.peek();
-            int prevHeight = top.height;
-            // System.out.printf("height %d, prev height %d", height, prevHeight);
-            
+            prevHeight = stackSize == 0 ? -1 : stack.peek().height;
             if(height > prevHeight) {
-                stack.push(new Node(height, i));
-               // System.out.printf("Adding height %d, startInd %d", height, i);
-            }
-            else if(height < prevHeight){ // Height less then top
-                // remove items from stack, compute max
-                // System.out.printf("Height reduction %d%n", heightReduction);
-                // System.out.printf("Height less: current %d previous %d%n", height, prevHeight);
-                
-                int size = 0;
-                while(!stack.isEmpty()) {
-                    Node currTop = stack.pop();
-                    res =  updateMax(res, computeHeightSquare(currTop.height, currTop.index, i-1));
-                    ++size;
-                }
-                if(height > 0) {
-                    stack.push(new Node(height, i-size));
-                }
+                ++stackSize;
+                stack.push(new Node(height, itemInd));
             }
         }
         while(!stack.isEmpty()) {
             Node top = stack.pop();
-            // System.out.printf("%d %d %d%n", top.height, top.index, h.length - 1);
             res = updateMax(res, computeHeightSquare(top.height, top.index, h.length));
         }
         return res;
